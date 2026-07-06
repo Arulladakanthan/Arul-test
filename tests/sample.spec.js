212190ad90a05065ed0f46b652ccd410adcb92ac
Arulladakanthan <arulladakanthan@gmail.com>
2026-07-06 17:35:37 +0530

    final update

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -4,4 +4,4 @@ test('Launch a URL',async ({page})=>{
     await page.goto("https://www.amazon.in/?&tag=googhydrabk1-21&ref=pd_sl_7hz2t19t5c_e&adgrpid=155259815513&hvpone=&hvptwo=&hvadid=813766258703&hvpos=&hvnetw=g&hvrand=15345218883572627417&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9061917&hvtargid=kwd-10573980&hydadcr=14453_2461882&gad_source=1");
 })
 
-mmm
+ooooo
